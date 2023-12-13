5aa61d26ed2c3c393c6c139cfa5fc91f364b6df0
Hugo Barros <hugosbarros@gmail.com>
2023-12-13 15:57:32 +0000

    fixed

--- binary_search-final.py
+++ binary_search-final.py
@@ -17,7 +17,7 @@ def binary_search(arr, target, low, high):
     while low <= high:
         mid = low + (high - low) // 2
 
-        if arr[mid] = target:
+        if arr[mid] == target:
             return mid
 
         elif arr[mid] < target:
